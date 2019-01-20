e6f948955cb32895927494cee72a012d2b4be07f
TimothyYeou <timyeou1234@hotmail.com>
2019-01-20 17:16:41 +0800

    Add test case for data process to Model

--- iOS_Exam/iOS_ExamTests/RandomContactServiceTest.swift
+++ iOS_Exam/iOS_ExamTests/RandomContactServiceTest.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import Alamofire
+import Mockingjay
 @testable import iOS_Exam
 
 class RandomContactServiceTest: XCTestCase {
@@ -15,11 +16,19 @@ class RandomContactServiceTest: XCTestCase {
     var randomContactService :RandomContactServiceProtocol!
     
     override func setUp() {
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         randomContactService = RandomContactService()
+        
+        let url = Bundle(for: type(of: self)).url(forResource: "UserJson", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        
+        stub(uri("api/TestSuccess"), jsonData(data))
+        
     }
 
     override func tearDown() {
+        super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         randomContactService = nil
     }
@@ -48,12 +57,35 @@ class RandomContactServiceTest: XCTestCase {
         // 3
         waitForExpectations(timeout: 5, handler: nil)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testNormalRespones() {
+        // given
+        let url = URL(string: "api/TestSuccess")
+        let promise = expectation(description: "Data Proccess Success")
+        
+        // when
+        Alamofire.request(url!).responseJSON {
+            (response) in
+            if let error = response.error{
+                ///獲取失敗，網路錯誤
+                XCTFail("Error: \(error.localizedDescription)")
+            }
+            if let statusCode = response.response?.statusCode{
+                if statusCode == 200 {
+                    if ((RandomContactService().decodeJsonData(response))?.count)! > 0{
+                        promise.fulfill()
+                    }else{
+                        XCTFail("資料解析錯誤")
+                    }
+                }else{
+                    XCTFail("statusCode: \(statusCode)")
+                }
+            }
         }
+        
+        // 3
+        waitForExpectations(timeout: 5, handler: nil)
+        
     }
-
+    
 }
